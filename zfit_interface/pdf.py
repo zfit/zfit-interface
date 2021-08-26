f9931590dcab3712ec3576333b36ce202a0018ea
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-08-27 00:00:38 +0200

    enh: add var input types

--- zfit_interface/pdf.py
+++ zfit_interface/pdf.py
@@ -5,13 +5,49 @@ from zfit_interface.typing import VarInputType
 
 
 class ZfitPDF(ZfitFunc):
-    def pdf(self, var: VarInputType, norm: NormInputType = None) -> ArrayLike:
+    def pdf(
+        self, var: VarInputType, norm: NormInputType = None, *, options=None
+    ) -> ArrayLike:
+        """Probability density of the defined function, normalized over *norm* to 1."""
         raise NotImplementedError
 
-    def integrate(self, limits, *, norm=None, var=None):
+    def ext_pdf(
+        self, var: VarInputType, norm: NormInputType = None, *, options=None
+    ) -> ArrayLike:
+        """Probability density of the defined function, normalized over *norm* to the yield.
+
+        This method is only available for extended PDFs.
+        """
         raise NotImplementedError
 
-    def sample(self, n, limits=None, *, var):
+    def counts(
+        self, var: VarInputType, norm: NormInputType, *, options=None
+    ) -> ArrayLike:
+        """Counts of a histogram, corresponds to the integral over *ext_pdf* for the bin edges.
+
+        This method corresponds to the "frequency" of a histogram.
+
+        This method is only available for extended PDFs.
+        """
+        raise NotImplementedError
+
+    def rel_counts(
+        self, var: VarInputType, norm: NormInputType, *, options=None
+    ) -> ArrayLike:
+        """Relative counts of a histogram, corresponds to the integral over *pdf* for the bin edges.
+
+        This method corresponds to the "relative frequency" of a histogram.
+        """
+        raise NotImplementedError
+
+    def integrate(self, limits, *, norm=None, var=None, options=None):
+        """Integrate (analytically, otherwise numerically) over *limits*."""
+        raise NotImplementedError
+
+    def ext_integrate(self, limits, *, norm=None, var=None, options=None):
+        raise NotImplementedError
+
+    def sample(self, n, limits=None, *, var=None, options=None):
         raise NotImplementedError
 
     @property
