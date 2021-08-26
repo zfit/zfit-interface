8d77fedd4c0413b482b2f1ea5959a354f2c9c6d2
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-08-27 00:00:38 +0200

    enh: add var input types

--- zfit_interface/func.py
+++ zfit_interface/func.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from collections.abc import Mapping
 
+from zfit_interface.typing import ArrayLike
+from zfit_interface.typing import VarInputType
 from zfit_interface.variables import ZfitVar
 
 
@@ -10,12 +12,15 @@ class ZfitFunc:
     def variables(self) -> Mapping[str, ZfitVar]:
         raise NotImplementedError
 
-    def __call__(self, var):
+    def __call__(self, var=None):
         raise NotImplementedError
 
     @property
     def output_variables(self) -> Mapping[str, ZfitVar]:
         raise NotImplementedError
 
-    def integrate(self, limits, *, var=None):
+
+class ZfitScalarFunc:
+    def values(self, var: VarInputType = None, *, options=None) -> ArrayLike:
+        """Values of a histogram, corresponds to the integral over *func* for the bin edges."""
         raise NotImplementedError
