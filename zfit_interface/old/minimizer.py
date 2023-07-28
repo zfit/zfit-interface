e1dda2cc85e6b5850616a72ffedaa935356c6150
Jonas Eschle <mayou36@jonas.eschle.com>
2023-07-27 22:02:29 -0400

    ci: upgrade cd to use pypi trusted publisher

--- zfit_interface/old/minimizer.py
+++ zfit_interface/old/minimizer.py
@@ -1,23 +1,17 @@
 from __future__ import annotations
 
-from abc import abstractmethod
 from collections.abc import Iterable
+from typing import Protocol
 
 from zfit_interface.old.loss import ZfitMinimalLoss
 from zfit_interface.old.param import ZfitParameter
 from zfit_interface.old.result import ZfitMinimalResult
 
 
-class ZfitMinimizer:
+class ZfitMinimizer(Protocol):
     """Define the minimizer interface."""
 
-    @abstractmethod
     def minimize(
         self, loss: ZfitMinimalLoss, params: Iterable[ZfitParameter] = None
     ) -> ZfitMinimalResult:
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def tolerance(self):
         raise NotImplementedError
